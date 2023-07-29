8802fc12433a6cf1e6690a002839615e858dc4bd
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-29 21:59:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 #!/usr/bin/env node
-'use strict';
-const app = require('express')();
-const params = require('./src/params');
+'use strict'
+const app = require('express')()
+const authenticate = require('./src/authenticate')
+const params = require('./src/params')
+const proxy = require('./src/proxy')
+//import app from 'express';
+//import authenticate from '../src/authenticate.js';
+//import params from '../src/param.js';
+//import proxy from '../src/proxy.js';
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
-app.enable('trust proxy');
-app.get('/params');
-app.get('/favicon.ico', (req, res) => res.status(204).end());
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.enable('trust proxy')
+app.get('/', authenticate, params, proxy)
+app.get('/favicon.ico', (req, res) => res.status(204).end())
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
