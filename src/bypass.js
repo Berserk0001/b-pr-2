5843b94da6b5227732da42648f55b68e2b4781a6
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-31 10:34:31 +0530

    Delete bypass.js

--- src/bypass.js
+++
@@ -1,7 +0,0 @@
-function bypass(req, res, buffer) {
-  res.setHeader('x-proxy-bypass', 1);
-  res.setHeader('content-length', buffer.length);
-  res.status(200).send(buffer);
-}
-
-module.exports = bypass;
